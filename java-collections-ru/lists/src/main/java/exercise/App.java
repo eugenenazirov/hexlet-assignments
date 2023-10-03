7544f2b953c8b5a954be6074e72517b6b99f5c59
Eugene Nazirov <jagzonmusic@gmail.com>
2023-10-03 11:54:05 +0600

    Add scrabble game exercise

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -5,5 +5,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
+public class App {
+    public static boolean scrabble(String charset, String neededWord) {
+        List<String> charsetList = getListFromString(charset);
+        List<String> neededWordList = getListFromString(neededWord);
 
+        for (String ch : neededWordList) {
+            if (!containsIgnoreCase(charsetList, ch)) {
+                return false;
+            }
+
+            charsetList.remove(ch);
+        }
+
+        return true;
+    }
+
+    private static List<String> getListFromString(String s) {
+        return new ArrayList<>(Arrays.asList(s.split("")));
+    }
+
+    private static boolean containsIgnoreCase(List<String> stringList, String ch) {
+        for (String str : stringList) {
+            if (str.equalsIgnoreCase(ch)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
 //END
